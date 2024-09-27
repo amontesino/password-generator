f7ef7bc7d46641536e9fb18b341a34152df19ac6
A. Montesino <120063688+amontesino@users.noreply.github.com>
2024-09-27 17:57:16 -0400

    used concat and slice to properly initialize array with minimal requirements (letters) and add to it, rendered them onto text fields, implemented length slider as a changeable state value, used said state value to control password length, added ability to copy password field content to clipboard

--- src/components/container.tsx
+++ src/components/container.tsx
@@ -1,56 +1,99 @@
-import { useEffect, useRef, useState } from "react"
-import { characters, charactersNoNumbers, charactersNoNumbersNoSymbols, charactersNoSymbols } from "../data/characters"
+import { useState } from "react";
+import { characters } from "../data/characters";
 
 export default function Container() {
-    const [numberCheck, setNumberCheck] = useState(false)
-    const [symbolCheck, setSymbolCheck] = useState(false)
-    const [characterArray, setCharacterArray] = useState([])
+  const [numberCheck, setNumberCheck] = useState(false);
+  const [symbolCheck, setSymbolCheck] = useState(false);
+  const [passwords, setPasswords] = useState([]);
+  const [length, setLength] = useState(15);
 
-    function genPass(array: Array<string>) {
-        // console.log(array)
-        // console.log(`symbol check: ${symbolCheck}, number check: ${numberCheck}`)
+  const genPass = () => {
+    // Uppercase and lowercase letters
+    let selectedChars = characters.slice(0, 52);
+    let passwordOne = "";
+    let passwordTwo = "";
+
+    // Add numbers if numberCheck is true
+    if (numberCheck) {
+      selectedChars = selectedChars.concat(characters.slice(52, 62));
     }
 
-    useEffect(() => {
-        console.log(characterArray)
-        console.log(`symbol check: ${symbolCheck}, number check: ${numberCheck}`)
+    // Add symbols if symbolCheck is true
+    if (symbolCheck) {
+      selectedChars = selectedChars.concat(characters.slice(62));
+    }
 
-        return () => {
-            if (symbolCheck === false && numberCheck) {
-                setCharacterArray(charactersNoSymbols)
-            } else
-            if (numberCheck === false && symbolCheck) {
-                setCharacterArray(charactersNoNumbers)
-            } else 
-            if (numberCheck === false && symbolCheck === false) {
-                setCharacterArray(charactersNoNumbersNoSymbols)
-            } else {
-                setCharacterArray(characters)
-            }
-        }
-    }, [numberCheck, symbolCheck])
+    console.log(selectedChars);
+    console.log(`symbol check: ${symbolCheck}, number check: ${numberCheck}`);
 
-    return (
-        <div className="container">
-            <h1>Generate a</h1>
-            <h1 className="header">random password</h1>
-            <h2>Never use an insecure password again.</h2>
-            <input type="range" min="8" max="32" placeholder="15" className="slider" id="pass-length" />
-            <p id="output-length"></p>
-            <div className="checkboxes">
-                <input type="checkbox" id="symbol-check" onChange={() => setSymbolCheck(prevCheck => !prevCheck)} />
-                <label htmlFor="symbol-check">Use symbols?</label>
-                <input type="checkbox" id="number-check" onChange={() => setNumberCheck(prevCheck => !prevCheck)} />
-                <label htmlFor="number-check">Use numbers?</label>
-            </div>
-            <button onClick={() => genPass(characterArray)}>Generate passwords</button>
-            <div id="line"></div>
-            <div className="fields">
-                <div className="passField" id="pass-one"></div>
-                <div className="passField" id="pass-two"></div>
-            </div>
-            <p id="copy-confirm">Click to copy passwords!</p>
-            <script src="index.js"></script>
+    for (let i = 0; i < length; i++) {
+      passwordOne =
+        passwordOne +
+        selectedChars[Math.floor(Math.random() * selectedChars.length)];
+      passwordTwo =
+        passwordTwo +
+        selectedChars[Math.floor(Math.random() * selectedChars.length)];
+    }
+
+    console.log(passwordOne, passwordTwo);
+    setPasswords([passwordOne, passwordTwo]);
+  };
+
+  return (
+    <div className='container'>
+      <h1>Generate a</h1>
+      <h1 className='header-purple'>random password</h1>
+      <h2>Never use an insecure password again.</h2>
+      <input
+        type='range'
+        min='8'
+        max='32'
+        placeholder={JSON.stringify(length)}
+        className='slider'
+        id='pass-length'
+        onChange={(e) => setLength(e.target.value)}
+      />
+      <p id='output-length'></p>
+      <div className='checkboxes'>
+        <label htmlFor='symbol-check'>
+          <input
+            type='checkbox'
+            id='symbol-check'
+            onChange={() => setSymbolCheck((prevCheck) => !prevCheck)}
+          />
+          <span className='checkmark' />
+          Use symbols?
+        </label>
+        <label htmlFor='number-check'>
+          <input
+            type='checkbox'
+            id='number-check'
+            onChange={() => setNumberCheck((prevCheck) => !prevCheck)}
+          />
+          <span className='checkmark' />
+          Use numbers?
+        </label>
+      </div>
+      <button onClick={() => genPass()}>Generate passwords</button>
+      <div id='line'></div>
+      <div className='fields'>
+        <div
+          className='passField'
+          id='pass-one'
+          onClick={() => navigator.clipboard.writeText(passwords[0])}
+        >
+          {passwords[0]}
         </div>
-    )
+        <div
+          className='passField'
+          id='pass-two'
+          onClick={() => navigator.clipboard.writeText(passwords[1])}
+        >
+          {passwords[1]}
+        </div>
+      </div>
+      <p id='copy-confirm'>Click to copy passwords!</p>
+      <script src='index.js'></script>
+    </div>
+  );
 }
